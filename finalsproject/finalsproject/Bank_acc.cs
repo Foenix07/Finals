47d8033d2026f9f38459dad3a3f914e0332b88ba
Foenix07 <149000720+Foenix07@users.noreply.github.com>
2024-02-20 23:49:04 +0100

    Complete list of properties

--- finalsproject/finalsproject/Bank_acc.cs
+++ finalsproject/finalsproject/Bank_acc.cs
@@ -8,14 +8,20 @@ namespace finalsproject
 {
     internal class Bank_acc
     { 
-        public int accID;
+        public string accID;
         public string accNumber;
         public string holder;
         public int holderAge;
         public bool holderParentalControl;
         public string holderEmail;
         public string holderPhone;
-
+	public int balance;
+	public int loan;
+	public int loanInterestRate;
+	public int dailyLimit;
+	public string password;
+	public enum accType;
+	public int pin;
 
     }
 }
