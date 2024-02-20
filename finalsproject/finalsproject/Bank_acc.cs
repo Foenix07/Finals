cbdf079ab0c53a5a349342f9f2442d4a8f721c14
Foenix07 <149000720+Foenix07@users.noreply.github.com>
2024-02-20 16:03:33 +0100

    start

---
+++ finalsproject/finalsproject/Bank_acc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalsproject
+{
+    internal class Bank_acc
+    { 
+        public int accID;
+        public string accNumber;
+        public string holder;
+        public int holderAge;
+        public bool holderParentalControl;
+        public string holderEmail;
+        public string holderPhone;
+
+
+    }
+}
