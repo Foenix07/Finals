6cd3798285627c287d5645cecdc55bbd018c8f4e
Daniela Suchánková <daniela.suchankovaa@gmail.com>
2024-02-28 08:02:54 +0100

    pridano na skolni pc

--- finalsproject/finalsproject/Program.cs
+++ finalsproject/finalsproject/Program.cs
@@ -13,8 +13,9 @@ namespace finalsproject
             string acctype = Console.ReadLine();
             Bank_acc bankacc1 = new Bank_acc("ahoj", "1234567", "Dani", 18, true, "d.d@gmail.com", "password", "12345678", 1098.345, 12345.345678, 5.7, 50000, "1234567898765432", 2353, AccType: int.Parse(acctype));
             Console.WriteLine(bankacc1.accType);
+            Console.WriteLine("Hello World!");
             Console.ReadKey();
-
+            
         }
     }
 }
