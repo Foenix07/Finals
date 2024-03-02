c3b4129c1db3d9dbf9138d9f0e9126cb568cf096
Foenix07 <149000720+Foenix07@users.noreply.github.com>
2024-03-02 22:54:21 +0100

    dk wtf im doinnng

--- finalsproject/finalsproject/Bank_acc.cs
+++ finalsproject/finalsproject/Bank_acc.cs
@@ -31,23 +31,23 @@ namespace finalsproject
         public int pin;
 	    public AccTypeEnum accType;
 	    
-        public Bank_acc(string AccID, string AccNumber, string Holder, int HolderAge, bool HolderParentalControl, string HolderEmail, string Password, string HolderPhone, double Balance, double Loan, double LoanInterestRate, double DailyLimit, string CardNumber, int Pin, int AccType)
+        public Bank_acc(string AccID, string AccNumber, string Holder, int HolderAge, bool HolderParentalControl, string HolderEmail, string Password, string HolderPhone, double Balance, double Loan, double LoanInterestRate, double DailyLimit, string CardNumber, int Pin, AccTypeEnum AccType)
         {
-            accID = AccID;
-            accNumber = AccNumber;
-            holder = Holder;
-            holderAge = HolderAge;
-            holderParentalControl = HolderParentalControl;
-            holderEmail = HolderEmail;
-            password = Password;
-            holderPhone = HolderPhone;
-            balance = Balance;
-            loan = Loan;
-            loanInterestRate = LoanInterestRate;
-            dailyLimit = DailyLimit;
-            cardNumber = CardNumber;
-            pin = Pin;
-            accType = (AccTypeEnum)AccType;
+            this.accID = AccID;
+            this.accNumber = AccNumber;
+            this.holder = Holder;
+            this.holderAge = HolderAge;
+            this.holderParentalControl = HolderParentalControl;
+            this.holderEmail = HolderEmail;
+            this.password = Password;
+            this.holderPhone = HolderPhone;
+            this.balance = Balance;
+            this.loan = Loan;
+            this.loanInterestRate = LoanInterestRate;
+            this.dailyLimit = DailyLimit;
+            this.cardNumber = CardNumber;
+            this.pin = Pin;
+            this.accType = AccType;
         }
         
 
