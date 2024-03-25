90e2f451c2ff7c2caf3fcea236db7f2e10942c0a
Foenix07 <149000720+Foenix07@users.noreply.github.com>
2024-03-25 22:11:28 +0100

    yeah no idea again

---
+++ finalsproject/finalsproject/Bank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalsproject
+{
+    public class Bank
+    {
+        //a new dictionary
+        Dictionary<string, Bank_acc> bankaccounts = new Dictionary<string, Bank_acc>();
+
+        public void MakeAcc()
+        {
+            string acctype = Console.ReadLine();
+            Bank_acc bankacc1 = new Bank_acc("ahoj", "1234567", "Dani", 18, true, "d.d@gmail.com", "password", "12345678", 1098.345, 12345.345678, 5.7, 50000, AccType: int.Parse(acctype));
+            bankaccounts.Add(bankacc1.accID, bankacc1);
+            bankacc1.GetThingies(bankaccounts[bankacc1.accID]);
+        }
+
+    }
+}
